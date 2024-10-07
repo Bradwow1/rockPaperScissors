ecccb7f53a47e28cde5decb15c06bcec0608d602
Bucheit <25BucheitB@badinhs.org>
2024-10-07 14:15:09 -0400

    created rockPaperScissors.js and added instructions as comments

---
+++ rockPaperScissors.js
@@ -0,0 +1,27 @@
+// Step 1:
+// test that your javascript file is linked to your HTML file by console.log("Hello, world!");
+
+// Step 2: 
+// Create a function called getComputerChoice
+// -getComputerChoice will randomly return "rock", "paper", "scissors"
+// -HINT: Math.random() is a function that returns a number between 0 and 1.  You can use this to make random selections.
+// -Once you think you've got this working, test it by console.log(getComputerChoice());
+
+// Step 3:
+// Create a function called getHumanChoice:
+// -Write the code so getHumanChoice returns one of "rock", "paper", or "scissors" based on user input
+// -HINT: prompt() is a function that will allow you to get user input
+// -Once you think you've got this working, test it by console.log(getHumanChoice());
+
+// Step 4:
+// Score tracking
+// -Create two variables to score human and computer scores
+// -initialize these variables with a score of 0
+
+// Step 5: 
+// Write the logic to play one game
+// -Create a function called playRound
+// -2 parameters (humanChoice, computerChoice)
+// -make sure that human choice logic is case-insensitive (rock, RoCK, RocK, ROCK should all work equally well)
+// -Should console.log a message like "You win! Rock beats scissors")
+// -increment the human/computer score based on winner
